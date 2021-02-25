b2ce65ab481da5c934bae66edbb9d34de7aba842
narumincho <narumincho.starfy@gmail.com>
2021-02-25 22:44:12 +0900

    fix: renderState で class 構文を使用しない. (Definyでのエラー防止)

--- renderState.ts
+++ renderState.ts
@@ -1,99 +1,103 @@
 import * as v from "./view";
 
-export class RenderState<Message> {
-  private messageData: v.MessageData<Message> = {
+export type RenderState<Message> = {
+  readonly clickEventHandler: (path: string, mouseEvent: MouseEvent) => void;
+  readonly changeEventHandler: (path: string) => void;
+  readonly inputEventHandler: (path: string, inputEvent: InputEvent) => void;
+  readonly setMessageDataMap: (messageData: v.MessageData<Message>) => void;
+  readonly pointerMoveHandler: (pointerEvent: PointerEvent) => void;
+  readonly pointerDownHandler: (pointerEvent: PointerEvent) => void;
+};
+
+/**
+ * イベントを受け取る関数の指定をして, ブラウザで描画する前の準備をする
+ *  @param messageHandler メッセージーを受け取る関数
+ */
+export const useRenderState = <Message>(
+  messageHandler: (message: Message) => void
+): RenderState<Message> => {
+  let messageData: v.MessageData<Message> = {
     messageMap: new Map(),
     pointerMove: undefined,
     pointerDown: undefined,
   };
 
-  /**
-   * イベントを受け取る関数の指定をして, ブラウザで描画する前の準備をする
-   *  @param messageHandler メッセージーを受け取る関数
-   */
-  constructor(private messageHandler: (message: Message) => void) {
-    this.messageHandler = messageHandler;
-  }
-
-  clickEventHandler(path: string, mouseEvent: MouseEvent): void {
-    const eventData = this.messageData.messageMap.get(path);
-    if (eventData === undefined) {
-      return;
-    }
-    const messageData = eventData.onClick;
-    if (messageData === undefined) {
-      return;
-    }
-    if (messageData.ignoreNewTab) {
-      /*
-       * リンクを
-       * Ctrlなどを押しながらクリックか,
-       * マウスの中ボタンでクリックした場合などは, ブラウザで新しいタブが開くので, ブラウザでページ推移をしない.
-       */
-      if (
-        mouseEvent.ctrlKey ||
-        mouseEvent.metaKey ||
-        mouseEvent.shiftKey ||
-        mouseEvent.button !== 0
-      ) {
+  return {
+    clickEventHandler(path: string, mouseEvent: MouseEvent): void {
+      const eventData = messageData.messageMap.get(path);
+      if (eventData === undefined) {
         return;
       }
-      mouseEvent.preventDefault();
-    }
-    if (messageData.stopPropagation) {
-      mouseEvent.stopPropagation();
-    }
-    this.messageHandler(messageData.message);
-  }
-
-  changeEventHandler(path: string): void {
-    const eventData = this.messageData.messageMap.get(path);
-    if (eventData === undefined) {
-      return;
-    }
-    const messageData = eventData.onChange;
-    if (messageData === undefined) {
-      return;
-    }
-    this.messageHandler(messageData);
-  }
-
-  inputEventHandler(path: string, inputEvent: InputEvent): void {
-    const eventData = this.messageData.messageMap.get(path);
-    if (eventData === undefined) {
-      return;
-    }
-    const messageData = eventData.onInput;
-    if (messageData === undefined) {
-      return;
-    }
-    this.messageHandler(
-      messageData(
-        (inputEvent.target as HTMLInputElement | HTMLTextAreaElement).value
-      )
-    );
-  }
-
-  setMessageDataMap(messageData: v.MessageData<Message>): void {
-    this.messageData = messageData;
-  }
-
-  pointerMoveHandler(pointerEvent: PointerEvent): void {
-    if (this.messageData.pointerMove !== undefined) {
-      this.messageHandler(
-        this.messageData.pointerMove(pointerEventToPointer(pointerEvent))
+      const clickMessage = eventData.onClick;
+      if (clickMessage === undefined) {
+        return;
+      }
+      if (clickMessage.ignoreNewTab) {
+        /*
+         * リンクを
+         * Ctrlなどを押しながらクリックか,
+         * マウスの中ボタンでクリックした場合などは, ブラウザで新しいタブが開くので, ブラウザでページ推移をしない.
+         */
+        if (
+          mouseEvent.ctrlKey ||
+          mouseEvent.metaKey ||
+          mouseEvent.shiftKey ||
+          mouseEvent.button !== 0
+        ) {
+          return;
+        }
+        mouseEvent.preventDefault();
+      }
+      if (clickMessage.stopPropagation) {
+        mouseEvent.stopPropagation();
+      }
+      messageHandler(clickMessage.message);
+    },
+    changeEventHandler(path: string): void {
+      const eventData = messageData.messageMap.get(path);
+      if (eventData === undefined) {
+        return;
+      }
+      const changeMessage = eventData.onChange;
+      if (changeMessage === undefined) {
+        return;
+      }
+      messageHandler(changeMessage);
+    },
+    inputEventHandler(path: string, inputEvent: InputEvent): void {
+      const eventData = messageData.messageMap.get(path);
+      if (eventData === undefined) {
+        return;
+      }
+      const inputMessage = eventData.onInput;
+      if (inputMessage === undefined) {
+        return;
+      }
+      messageHandler(
+        inputMessage(
+          (inputEvent.target as HTMLInputElement | HTMLTextAreaElement).value
+        )
       );
-    }
-  }
-
-  pointerDownHandler(pointerEvent: PointerEvent): void {
-    if (this.messageData.pointerDown !== undefined) {
-      this.messageHandler(
-        this.messageData.pointerDown(pointerEventToPointer(pointerEvent))
-      );
-    }
-  }
-}
+    },
+    setMessageDataMap(newMessageData: v.MessageData<Message>): void {
+      messageData = newMessageData;
+    },
+    pointerMoveHandler(pointerEvent: PointerEvent): void {
+      if (messageData.pointerMove !== undefined) {
+        messageHandler(
+          messageData.pointerMove(pointerEventToPointer(pointerEvent))
+        );
+      }
+    },
+    pointerDownHandler(pointerEvent: PointerEvent): void {
+      if (messageData.pointerDown !== undefined) {
+        messageHandler(
+          messageData.pointerDown(pointerEventToPointer(pointerEvent))
+        );
+      }
+    },
+  };
+};
 
 const pointerEventToPointer = (pointerEvent: PointerEvent): v.Pointer => {
   return {
